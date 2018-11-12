25a32af025b5a228612b82b7652335d95ec3efac
NEWGEN\arushi-jain <arushi-jain@newgen.co.in>
2018-11-12 11:34:03 +0530

    Initial commit

---
+++ src/components/Fliplogin.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="flip-container"
+       :class="{ 'flipit': flipit }"
+       :style="{ width: width, height: height }"
+       >
+    <div class="flipper cardStyle">
+      <div class="front cardStyle">
+        <slot name="front">
+          </slot>
+      </div>
+      <div class="back cardStyle">
+        <slot name="back">
+        </slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    'width': {
+      type: String,
+      required: true
+    },
+    'height': String,
+    'flipit': {
+      type: Boolean,
+      required: true,
+      default: false
+    }
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+.cardStyle {
+    height: 50%;
+    width : 100%;
+    transition : '0.5s';
+}
+ .flip-container {
+    perspective: 1000;
+  }
+  .flip-container.flipit .flipper,
+  .flip-container.hover .flipper {
+    transform: rotateY(180deg);
+  }
+  .flipper {
+    transition: 0.6s;
+    transform-style: preserve-3d;
+    position: relative;
+  }
+  .front, .back {
+    backface-visibility: hidden;
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+  .front {
+    z-index: 2;
+    -webkit-transform: rotateY(0);
+    -moz-transform: rotateY(0);
+    -o-transform: rotateY(0);
+    transform: rotateY(0);
+  }
+  .back {
+    -webkit-transform: rotateY(180deg);
+    -moz-transform: rotateY(180deg);
+    -o-transform: rotateY(180deg);
+    transform: rotateY(180deg);
+  }
+</style>
---
+++ src/pages/fliploginimplementation.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="flex flex-center q-pt-xl">
+    <flip-login :flipit="flipit" width= "40%">
+   <div slot="front">
+     <q-card style="width: 80%;height : 50%;">
+      <q-card-title>
+        Login
+      </q-card-title>
+      <q-card-separator />
+      <q-card-main>
+        <q-input v-model="email" type="email" suffix="@gmail.com" float-label="Type Your Email" />
+        <q-input v-model="pass" type="password" float-label="Password" />
+        <q-btn class="q-mt-lg" color="secondary" @click = "flipit = !flipit">Login</q-btn><br>
+        <q-btn class="q-mt-md" color="secondary" @click = "flipit = !flipit">Not a member, Join now !</q-btn>
+     </q-card-main>
+    </q-card>
+   </div>
+   <div slot="back">
+    <q-card >
+      <q-card-title>
+        Sign Up
+      </q-card-title>
+      <q-card-separator />
+      <q-card-main>
+        <q-input v-model="name" type="text" float-label="First Name" />
+        <q-input v-model="email" type="email" suffix="@gmail.com" float-label="Type Google Email" />
+        <q-input v-model="pass1" type="password" float-label="Password" />
+        <q-input v-model="pass2" type="password" float-label="Confirm Password" />
+        <q-btn  class="q-mt-lg" color="secondary" @click = "flipit = !flipit">Sign Up</q-btn>
+      </q-card-main>
+    </q-card>
+  </div>
+</flip-login>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+import FlipLogin from '../components/Fliplogin'
+export default {
+  name: 'Auth',
+  data () {
+    return {
+      email: '',
+      show: true,
+      pass: '',
+      flipit: false,
+      name: '',
+      pass1: '',
+      pass2: ''
+    }
+  },
+  components: {
+    FlipLogin
+  }
+}
+</script>
